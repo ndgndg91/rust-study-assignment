707cb8e9245a248bea4346d8c03c4a9376d3becf
ndgndg91 <ndgndg91@gmail.com>
2021-05-16 21:57:35 +0900

    <chore> organizing modules

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,20 +1,9 @@
+use server::Server;
+
+mod server;
+mod http;
+
 fn main() {
     let server = Server::new(String::from("127.0.0.1:8080"));
     server.run();
-}
-
-struct Server {
-    addr: String,
-}
-
-impl Server {
-    fn new(addr: String) -> Self {
-        Self {
-            addr: addr
-        }
-    }
-
-    fn run(self) {
-        println!("Listening on {}", self.addr);
-    }
 }
