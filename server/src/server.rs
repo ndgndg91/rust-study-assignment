af86f66f6bf2d189808a7d3b7986a0238bc130d1
ndgndg91 <ndgndg91@gmail.com>
2021-05-17 21:49:27 +0900

    <feat> Result Enum, loop, match, tuples

--- server/src/server.rs
+++ server/src/server.rs
@@ -1,3 +1,5 @@
+use std::net::TcpListener;
+
 pub struct Server {
     addr: String,
 }
@@ -9,5 +11,16 @@ impl Server {
 
     pub fn run(self) {
         println!("Listening on {}", self.addr);
+
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {            
+            let (stream, addr) = match listener.accept() {
+                Ok((stream, addr)) => {
+                    (stream, addr)
+                },
+                Err(err) => println!("Failed to establish a connection : {}", err)
+            };
+        }
     }
 }
