53bb17e28ece314e22d43eb14a616928cf9d2ce7
ndgndg91 <ndgndg91@gmail.com>
2021-05-21 21:58:52 +0900

    <feat> routing incoming requests

--- server/src/website_handler.rs
+++ server/src/website_handler.rs
@@ -1,11 +1,19 @@
 use super::server::Handler;
-use super::http::{Request, Response, StatusCode};
+use super::http::{Request, Response, StatusCode, Method};
 
 pub struct WebSiteHandler;
 
 impl Handler for WebSiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
-        Response::new(StatusCode::Ok, Some("<h1>Test</h1>".to_string()))
+        match request.method() {
+            Method::GET => match request.path() {
+                "/" => Response::new(StatusCode::Ok, Some("<h1>Come on</h1>".to_string())),
+                "/hello" => Response::new(StatusCode::Ok, Some("<h1>hello</h1>".to_string())),
+                _ => Response::new(StatusCode::NotFound, None),
+            }
+            _ => Response::new(StatusCode::NotFound, None),
+        }
+    
     }
 
 }
