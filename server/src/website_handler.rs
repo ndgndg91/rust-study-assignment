80c849279cea60e851d52c1c92c450ac0f6253b0
ndgndg91 <ndgndg91@gmail.com>
2021-05-21 22:34:34 +0900

    <feat> serving arbitrary files securely

--- server/src/website_handler.rs
+++ server/src/website_handler.rs
@@ -12,8 +12,20 @@ impl WebSiteHandler {
     }
 
     fn read_file(&self, file_path: &str) -> Option<String> {
-        let path =format!("{}/{}", self.public_path, file_path);
-        fs::read_to_string(path).ok()
+        let path = format!("{}/{}", self.public_path, file_path);
+        
+        // remove .. in path
+        match fs::canonicalize(path) {
+            Ok(path) => {
+                if path.starts_with(&self.public_path) {
+                    fs::read_to_string(path).ok()
+                } else {
+                    println!("Directory Traversal ATtack Atemmpted: {}", file_path);
+                    None
+                }
+            },
+            Err(_) => None
+        }
     }
 }
 
@@ -23,7 +35,10 @@ impl Handler for WebSiteHandler {
             Method::GET => match request.path() {
                 "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
                 "/hello" => Response::new(StatusCode::Ok, Some("<h1>hello</h1>".to_string())),
-                _ => Response::new(StatusCode::NotFound, None),
+                path => match self.read_file(path) {
+                    Some(contents) => Response::new(StatusCode::Ok, Some(contents)),
+                    None => Response::new(StatusCode::NotFound, None)
+                },   
             }
             _ => Response::new(StatusCode::NotFound, None),
         }
